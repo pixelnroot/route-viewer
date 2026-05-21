97fd123f2f2451e0fdf8b669089820283b4df3df
Pixelnroot <worknova@pm.me>
2026-05-22 02:19:55 +0600

    Fixed the server pointing issue

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'field-data',
-      script: 'server.js',
+      name: "field-data",
+      script: "server.js",
       cwd: __dirname,
       instances: 1,
-      exec_mode: 'fork',
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 9898,
-        HOSTNAME: '0.0.0.0',
+        HOSTNAME: "127.0.0.1",
       },
     },
   ],
